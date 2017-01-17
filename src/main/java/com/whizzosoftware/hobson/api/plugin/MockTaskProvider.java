b6be0b0a2ec58bb90119a82394451d0f0ab3eefe
Dan Noguerol <dan@whizzosoftware.com>
2017-01-17 16:34:14 -0700

    Consolidated task registration and task creation into single event.

--- src/main/java/com/whizzosoftware/hobson/api/plugin/MockTaskProvider.java
+++ src/main/java/com/whizzosoftware/hobson/api/plugin/MockTaskProvider.java
@@ -1,10 +1,12 @@
-/*******************************************************************************
+/*
+ *******************************************************************************
  * Copyright (c) 2015 Whizzo Software, LLC.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
+ *******************************************************************************
+*/
 package com.whizzosoftware.hobson.api.plugin;
 
 import com.whizzosoftware.hobson.api.task.TaskContext;
@@ -28,12 +30,6 @@ public class MockTaskProvider implements TaskProvider {
     @Override
     synchronized public void onRegisterTasks(Collection<TaskContext> tasks) {
         createdTasks.addAll(tasks);
-        notify();
-    }
-
-    @Override
-    public void onCreateTask(TaskContext ctx) {
-        createdTasks.add(ctx);
         notify();
     }
 
